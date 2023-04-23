782e4a82ecbbc700c441a1a883767f6f6d76c9b4
jaewoong2 <jwisgenius@naver.com>
2023-04-23 11:12:07 +0900

    :art: add router code & 404 page

--- src/components/pages/Star/index.tsx
+++ src/components/pages/Star/index.tsx
@@ -1,31 +1,65 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import FoldAbleSection from '@/components/blocks/FoldAbleSection'
 import usePostStar from '@/hooks/usePostStar'
-import { Star as TStar } from '@/types'
-import { useDisclosure } from '@chakra-ui/react'
+import { Button, useDisclosure } from '@chakra-ui/react'
 import ModalBase from '@/components/blocks/ModaBase'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
+import useStarState from '@/hooks/useStarState'
 import StarMain from './StarMain'
 import StarResult from './StarResult'
 
-const Star = () => {
-  const { data, mutate, isLoading, status } = usePostStar()
-  const { onOpen, ...rest } = useDisclosure()
+const Component404 = () => {
+  return (
+    <div className="w-full h-full min-h-[calc(100vh-80px)] flex justify-center items-center flex-col">
+      <p className="text-red-400 text-lg">잘못된 접근이에요 😭</p>
+      <Link className="p-3 hover:text-blue-500 text-blue-400 flex flex-col gap-1" to="/write">
+        경함 작성 하러가기
+        <Button>이동</Button>
+      </Link>
+    </div>
+  )
+}
 
-  useEffect(() => {
-    onOpen()
-  }, [onOpen])
+const Star = () => {
+  const navigation = useNavigate()
+  const { onOpen, ...rest } = useDisclosure()
+  const starState = useStarState()
+
+  const { data, mutate, isLoading } = usePostStar({
+    onMutate: () => {
+      navigation('result')
+      onOpen()
+    },
+    onSuccess: () => {},
+  })
 
   return (
     <>
-      <FoldAbleSection title="STAR 작성">
-        <StarMain postStar={(star: TStar) => mutate(star)} />
-      </FoldAbleSection>
-      {status !== 'idle' && (
-        <FoldAbleSection title="결과">
-          <StarResult content={data?.content} isLoading={isLoading} />
-        </FoldAbleSection>
-      )}
-      <ModalBase {...rest}>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <FoldAbleSection title="STAR 작성">
+              <StarMain {...starState} mutate={mutate} />
+            </FoldAbleSection>
+          }
+        />
+        <Route
+          path="/result"
+          element={
+            data ? (
+              <FoldAbleSection title="결과">
+                <StarResult content={data?.content} isLoading={isLoading} />
+              </FoldAbleSection>
+            ) : (
+              <Component404 />
+            )
+          }
+        />
+        <Route path="/feedback" element />
+        <Route path="*" element={<Component404 />} />
+      </Routes>
+      <ModalBase {...rest} title="자소서 생성중...">
         <div className="w-full border aspect-square flex justify-center items-center bg-slate-50">
           광고박스
         </div>
