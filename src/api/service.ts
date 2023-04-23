779942e9527f35d2eb01da738b72bdc1e8cacd11
jaewoong2 <jwisgenius@naver.com>
2023-04-23 21:48:47 +0900

    :ailen: 피드백 API 요청 코드 구현 및 에러 핸들링 코드 추가

--- src/api/service.ts
+++ src/api/service.ts
@@ -1,8 +1,8 @@
-import { Star } from '@/types'
+import { Star, Feedback } from '@/types'
 import { AxiosError } from 'axios'
 import instance from './instance'
 
-export const postStar = async (star: Star) => {
+export const postStar = async (star: Star & { temperature: number }) => {
   try {
     const response = await instance.post<{
       role: 'assistant' | 'user' | 'system'
@@ -17,7 +17,31 @@ export const postStar = async (star: Star) => {
   } catch (err) {
     if (err instanceof AxiosError) {
       console.error(err.message)
+      throw err
     }
     console.error('Something Wrong...')
+    throw err
+  }
+}
+
+export const postFeedback = async (star: Feedback & { temperature: number }) => {
+  try {
+    const response = await instance.post<{
+      role: 'assistant' | 'user' | 'system'
+      content: string
+    }>('/openai/feedback', star)
+
+    if (response.status > 299) {
+      throw new AxiosError('Unknwon Error...')
+    }
+
+    return response.data
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      console.error(err.message)
+      throw err
+    }
+    console.error('Something Wrong...')
+    throw err
   }
 }
