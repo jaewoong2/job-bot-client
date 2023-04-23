50e424ed4676e0bd06ac71d93f1cd1d70dd89091
jaewoong2 <jwisgenius@naver.com>
2023-04-23 11:13:02 +0900

    :art: for user-experience

--- src/components/pages/Star/StarResult.tsx
+++ src/components/pages/Star/StarResult.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import useFoldAbleActions from '@/hooks/useFoldAbleActions'
+import { Button } from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import { RxArrowLeft } from 'react-icons/rx'
+import { Link } from 'react-router-dom'
 
 type Props = {
   content?: string
@@ -6,6 +10,12 @@ type Props = {
 }
 
 const StarResult = ({ content, isLoading }: Props) => {
+  const { show } = useFoldAbleActions()
+
+  useEffect(() => {
+    show()
+  }, [])
+
   return (
     <div className="mt-5 px-6 relative">
       {isLoading && (
@@ -16,6 +26,11 @@ const StarResult = ({ content, isLoading }: Props) => {
       {!isLoading && (
         <div className="border rounded-md w-full h-full min-h-[400px] p-3 py-8">{content}</div>
       )}
+      <div className="flex py-3">
+        <Link to="/write">
+          <Button leftIcon={<RxArrowLeft />}>이전</Button>
+        </Link>
+      </div>
     </div>
   )
 }
