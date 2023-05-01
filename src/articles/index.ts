41ea503c4deb2062881424fe83c80c0889bd784e
jaewoong2 <jwisgenius@naver.com>
2023-05-01 20:02:36 +0900

    :speech_balloon: article 관련 수정

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -1,6 +1,7 @@
 import * as Article1 from './article1.md'
 import * as Article2 from './article2.md'
+import * as Article3 from './article3.md'
 
 export default {
-  articles: [Article2, Article1],
+  articles: [Article2, Article1, Article3],
 }
--- src/components/pages/articles/index.tsx
+++ src/components/pages/articles/index.tsx
@@ -1,34 +1,43 @@
 import React, { useMemo } from 'react'
-import articles from '@/articles'
 import { useParams } from 'react-router-dom'
+import articles from '@/articles'
 import { SEO } from '@/components/blocks/SEO'
+import Error from '../Error'
 
 const Articles = () => {
   const params = useParams<{ article: string }>()
 
   const article = useMemo(() => {
-    return articles.articles.map(
-      ({ ReactComponent, attributes }) =>
-        attributes.title === decodeURIComponent(params.article!) && (
-          <div key={attributes.title}>
-            <SEO
-              description={attributes.description}
-              title={attributes.title}
-              name={attributes.title}
-            />
-            <div className="dark:bg-darkBg-400 dark:text-white p-6 markdown-body max-w-5xl mx-auto">
-              <div className="border-b mb-5">
-                <p className="text-3xl">{attributes.title}</p>
-                <div className="flex justify-between">
-                  <div>작성자: {attributes.author}</div>
-                  <p>작성날짜: {attributes.created}</p>
-                </div>
-              </div>
-              <ReactComponent />
-            </div>
-          </div>
-        )
+    const articleIndex = articles.articles.findIndex(
+      ({ attributes }) => attributes.title === decodeURIComponent(params.article!)
     )
+
+    if (articleIndex > -1) {
+      const targetArticle = articles.articles[articleIndex]
+
+      return (
+        <div key={targetArticle.attributes.title}>
+          <SEO
+            description={targetArticle.attributes.description}
+            title={targetArticle.attributes.title}
+            name={targetArticle.attributes.title}
+            image={targetArticle.attributes.img}
+          />
+          <div className="dark:bg-darkBg-400 dark:text-white p-6 markdown-body max-w-5xl mx-auto">
+            <div className="border-b mb-5">
+              <p className="text-3xl">{targetArticle.attributes.title}</p>
+              <div className="flex justify-between">
+                <div>작성자: {targetArticle.attributes.author}</div>
+                <p>작성날짜: {targetArticle.attributes.created}</p>
+              </div>
+            </div>
+            <targetArticle.ReactComponent />
+          </div>
+        </div>
+      )
+    }
+
+    return <Error>다양한 컨텐츠 즐기러 가기</Error>
   }, [articles])
 
   return <div>{article}</div>
