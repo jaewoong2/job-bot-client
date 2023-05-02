db4fa30d385c74600eee408ff5e4473b46519fa3
Lim Jaeung <lime@sk.com>
2023-05-02 17:06:30 +0900

    :sparkles: 쿠팡파트너스 배너 추가

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,10 +1,25 @@
 import Card from '@/components/atoms/Card'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { FcComments } from 'react-icons/fc'
 import { RxDividerVertical } from 'react-icons/rx'
 import articles from '@/articles'
 
 const Home = () => {
+  const coupangWrapper = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const coupangIframe = document.querySelector('.coupang-iframe')
+    const $parent = coupangIframe?.parentElement
+
+    if (coupangIframe) {
+      coupangWrapper?.current?.appendChild(coupangIframe)
+    }
+
+    return () => {
+      $parent?.appendChild(coupangIframe!)
+    }
+  }, [])
+
   return (
     <section className="flex flex-col gap-10">
       <div className="px-6 flex flex-col gap-5">
@@ -13,7 +28,10 @@ const Home = () => {
           <RxDividerVertical />
           <h2>꿀팁</h2>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-10 max-w-6xl w-full gap-5">
+        <div
+          className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-10 max-w-6xl w-full gap-5"
+          ref={coupangWrapper}
+        >
           {articles.articles.map(({ attributes }, i) => (
             <div key={`${attributes.title}-${+i}`} className="flex justify-center w-full">
               <Card
