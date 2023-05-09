1abeb91e003fd14558f955ba6381db238308508a
jaewoong2 <jwisgenius@naver.com>
2023-05-09 23:52:35 +0900

    :tada: migration to nextjs

--- src/components/atoms/Menu.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { components, MenuProps } from 'react-select'
-
-const Menu = ({ ...props }: MenuProps<any>) => {
-  return <components.Menu {...props} className="dark:bg-darkBg-500" />
-}
-
-export default Menu
