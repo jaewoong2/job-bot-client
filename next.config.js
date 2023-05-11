dbc1e5f9025cafcc8bd8f781dfe4cb853d730889
jaewoong2 <jwisgenius@naver.com>
2023-05-11 14:04:46 +0900

    feat: Deveploment 환경에서 Cors 를 위한 proxy 설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:54321/functions/v1/:path*',
+      },
+    ]
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
